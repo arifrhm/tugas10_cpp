6a9ef947761eb43c5f0e9ca061c75ad626f746d9
arifrhm <96586737+arifrhm@users.noreply.github.com>
2022-01-05 23:18:04 +0700

    Add files via upload

---
+++ Tugas10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int perkalian(){
+	cout<<"Selamat Datang Di Fungsi Perkalian"<<endl;
+	return 0;
+}
+
+int perkalian2(){
+	cout<<"Selamat Datang Di Fungsi Perkalian"<<endl;
+	return 0;
+}
+
+
+int main() {
+	perkalian();
+    return 0;
+}
